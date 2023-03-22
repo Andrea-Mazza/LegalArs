2a90f75f40fe079e3e298af69bb513788ad8e129
Andrea-Mazza <mazzaandrea45@gmail.com>
2023-03-22 16:22:25 +0100

    .

--- config/settings.py
+++ config/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = 'static/images'
 
