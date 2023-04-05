7a194229e1f1e752ed68df68e9a0f4d1c76e45ee
Andrea-Mazza <mazzaandrea45@gmail.com>
2023-04-05 08:14:40 +0200

    .

--- config/settings.py
+++ config/settings.py
@@ -92,66 +92,66 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DEVELOPMENT_MODE = False
 
-if DEVELOPMENT_MODE is True:
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = env('SECRET_KEY')
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = True
-    ALLOWED_HOSTS = ['192.168.1.88',
-                     '127.0.0.1', '192.168.0.167', '192.168.129.248', 'localhost']
-    DJANGO_SETTINGS_MODULE = 'config.settings'
-    EMAIL_BACKEND = env('EMAIL_BACKEND')
-    EMAIL_HOST = env('EMAIL_HOST')
-    EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = env('EMAIL_PORT')
-    EMAIL_USE_TLS = True
-    EMAIL_USE_SSL = env('EMAIL_USE_SSL')
-    STATIC_URL = 'static/'
-    STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+# if DEVELOPMENT_MODE is True:
+#     # SECURITY WARNING: keep the secret key used in production secret!
+#     SECRET_KEY = env('SECRET_KEY')
+#     # SECURITY WARNING: don't run with debug turned on in production!
+#     DEBUG = True
+#     ALLOWED_HOSTS = ['192.168.1.88',
+#                      '127.0.0.1', '192.168.0.167', '192.168.129.248', 'localhost']
+#     DJANGO_SETTINGS_MODULE = 'config.settings'
+#     EMAIL_BACKEND = env('EMAIL_BACKEND')
+#     EMAIL_HOST = env('EMAIL_HOST')
+#     EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+#     EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+#     EMAIL_PORT = env('EMAIL_PORT')
+#     EMAIL_USE_TLS = True
+#     EMAIL_USE_SSL = env('EMAIL_USE_SSL')
+#     STATIC_URL = 'static/'
+#     STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
-    MEDIA_ROOT = 'static/images'
+#     MEDIA_ROOT = 'static/images'
 
-    MEDIA_URL = '/images/'
-    STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
-    STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
-    BACKEND_DOMAIN = env('BACKEND_DOMAIN')
-    PAYMENT_SUCCESS_URL = env('PAYMENT_SUCCESS_URL')
-    PAYMENT_CANCEL_URL = env('PAYMENT_CANCEL_URL')
-else:
-    # SECURITY WARNING: keep the secret key used in production secret!
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    # SECURITY WARNING: don't run with debug turned on in production!
-    DEBUG = False
-    ALLOWED_HOSTS = ['legalars-app-9yyw9.ondigitalocean.app', 'localhost']
-    DJANGO_SETTINGS_MODULE = os.getenv('DJANGO_SETTINGS_MODULE')
-    EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
-    EMAIL_HOST = os.getenv('EMAIL_HOST')
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-    EMAIL_PORT = os.getenv('EMAIL_PORT')
-    EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
-    EMAIL_USE_SSL = False
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
-    AWS_S3_OBJECT_PARAMETERS = {
-        'CacheControl': 'max-age=86400',
-    }
-    AWS_LOCATION = 'static'
-    AWS_DEFAULT_ACL = 'public-read'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
-    STATIC_ROOT = 'static'
-    MEDIA_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, '/blog/')
-    MEDIA_ROOT = '/images/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
-    STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
-    BACKEND_DOMAIN = os.getenv('BACKEND_DOMAIN')
-    PAYMENT_SUCCESS_URL = os.getenv('PAYMENT_SUCCESS_URL')
-    PAYMENT_CANCEL_URL = os.getenv('PAYMENT_CANCEL_URL')
+#     MEDIA_URL = '/images/'
+#     STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+#     STRIPE_SECRET_KEY = env('STRIPE_SECRET_KEY')
+#     BACKEND_DOMAIN = env('BACKEND_DOMAIN')
+#     PAYMENT_SUCCESS_URL = env('PAYMENT_SUCCESS_URL')
+#     PAYMENT_CANCEL_URL = env('PAYMENT_CANCEL_URL')
+# else:
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY')
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+ALLOWED_HOSTS = ['legalars-app-9yyw9.ondigitalocean.app', 'localhost']
+DJANGO_SETTINGS_MODULE = os.getenv('DJANGO_SETTINGS_MODULE')
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_USE_SSL = False
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = os.getenv('AWS_S3_ENDPOINT_URL')
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+AWS_DEFAULT_ACL = 'public-read'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_ROOT = 'static'
+MEDIA_URL = '{}/{}/'.format(AWS_S3_ENDPOINT_URL, '/blog/')
+MEDIA_ROOT = '/images/'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STRIPE_PUBLISHABLE_KEY = os.getenv('STRIPE_PUBLISHABLE_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+BACKEND_DOMAIN = os.getenv('BACKEND_DOMAIN')
+PAYMENT_SUCCESS_URL = os.getenv('PAYMENT_SUCCESS_URL')
+PAYMENT_CANCEL_URL = os.getenv('PAYMENT_CANCEL_URL')
 
 if DEVELOPMENT_MODE is True:
     DATABASES = {
