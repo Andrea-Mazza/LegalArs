014804175aab390a8a0aeef925fd8cdbcf94d635
Andrea-Mazza <mazzaandrea45@gmail.com>
2023-04-07 17:44:33 +0200

    .

--- recuperoCredito/models.py
+++ recuperoCredito/models.py
@@ -177,7 +177,7 @@ class Fattura(models.Model):
 
 class ServizioRecuperoCredito(models.Model):
     current_user = models.ForeignKey(
-        CustomUser, on_delete=models.CASCADE, null=True, default='', blank=True, db_column='current_user_id')
+        CustomUser, on_delete=models.CASCADE, null=True, default=0, blank=True, db_column='current_user_id')
     time = models.DateTimeField(
         default=timezone.now)
     cr_tipo = models.CharField(max_length=1000, default='')
